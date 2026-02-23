95df190e5ee34df3a2733e8f29b1dd303c60b519
fat83dotcom <fernando.quimic@gmail.com>
2026-02-22 23:17:14 -0300

    Corrigindo propriedade do DTO

--- Application/Request/User/CreateUserRequest.cs
+++ Application/Request/User/CreateUserRequest.cs
@@ -2,7 +2,7 @@ namespace UseCase.Request.User;
 
 public class CreateUserRequest
 {
-    public string FirtsName { get; set; }
+    public string FirstName { get; set; }
     public string LastName { get; set; }
     public DateTime BirthDate { get; set; }
     public string Document { get; set; }

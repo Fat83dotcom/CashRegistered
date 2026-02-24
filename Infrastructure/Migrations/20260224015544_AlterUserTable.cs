72f619d437d064a55962322d221b19ca6153a1a2
fat83dotcom <fernando.quimic@gmail.com>
2026-02-23 22:59:37 -0300

    Migration para adaptar ao value object Name.

---
+++ Infrastructure/Migrations/20260224015544_AlterUserTable.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Repository.Migrations
+{
+    /// <inheritdoc />
+    public partial class AlterUserTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "LastName",
+                table: "Users",
+                newName: "Name_LastName");
+
+            migrationBuilder.RenameColumn(
+                name: "FirstName",
+                table: "Users",
+                newName: "Name_FirstName");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "Name_IsValid",
+                table: "Users",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Name_IsValid",
+                table: "Users");
+
+            migrationBuilder.RenameColumn(
+                name: "Name_LastName",
+                table: "Users",
+                newName: "LastName");
+
+            migrationBuilder.RenameColumn(
+                name: "Name_FirstName",
+                table: "Users",
+                newName: "FirstName");
+        }
+    }
+}
